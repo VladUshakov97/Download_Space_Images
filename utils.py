89b80e8bf3dd7df758bd21a606369645b2191e49
VladUshakov97 <vladushakov89@gmail.com>
2025-06-25 15:15:30 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -3,10 +3,6 @@ from pathlib import Path
 from urllib.parse import urlsplit, unquote
 from dotenv import load_dotenv
 
-def setup_env():
-    load_dotenv()
-    return os.getenv("NASA_API_KEY"), os.getenv("PATH_TO_IMAGE", "images")
-
 def get_image_extension(url):
     path = urlsplit(url).path
     path = unquote(path)
@@ -19,3 +15,14 @@ def save_image(content, folder, filename):
     with open(filepath, "wb") as f:
         f.write(content)
     print(f"Сохранено изображение: {filepath}")
+
+def main():
+    load_dotenv()  # Загрузка .env происходит здесь!
+    nasa_api_key = os.getenv("NASA_API_KEY")
+    save_path = os.getenv("PATH_TO_IMAGE", "images")
+    
+    # Пример вызова
+    print(f"API Key: {nasa_api_key}, save path: {save_path}")
+
+if __name__ == "__main__":
+    main()
